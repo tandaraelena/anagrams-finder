6793cfd2afecc3a30ee49f30e14c076c4cbc0412
Elena Tandara <tandaraelena@yahoo.com>
2024-06-14 13:59:35 +0100

    chore: restructure data

--- src/utils.ts
+++ src/utils.ts
@@ -11,3 +11,16 @@ export const fetchData = async (setData: React.Dispatch<React.SetStateAction<str
 };
 
 export const sanitiseWord = (word: string) => word.replace(/[^A-Za-z0-9]/g,'').toLowerCase().split('').join('');
+
+
+export const sanitiseWordsList = (words: string[]) => {
+  const newWordsStructure: Record<string, Array<string>> = {};
+
+  words.forEach((word) => {
+    const cleanWord = sanitiseWord(word);
+    if (!Object.hasOwn(newWordsStructure, cleanWord)) {
+      newWordsStructure[cleanWord] = [];
+    }
+    newWordsStructure[cleanWord].push(cleanWord);
+  });
+};
