5977d46ef4357bfeff7f45dfeb7ce5f1f3b2b81a
Elena Tandara <tandaraelena@yahoo.com>
2024-06-15 22:54:01 +0100

    chore: add anagrams file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Anagrams finder", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Anagrams finder/i);
   expect(linkElement).toBeInTheDocument();
 });
